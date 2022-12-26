79f4382ca65f32ded6d2f18c48c347b7daa9b932
matheuspungirum <matheus_pungirum@hotmail.com>
2022-12-26 02:03:42 -0300

    Problema da acentuação resolvida

---
+++ Projeto de Desenvolvimento de Aplicação para Tratamento de Dados.sql
@@ -0,0 +1,8 @@
+insert into projeto_de_desenvolvimento_de_aplicação_para_tratamento_de_dados
+(nome, nascimento, sexo, cidade, PDPPI, PDPPII95, PPDPIS95, PDB, IIB95, ISB95, NUNADB, NUNIIS95, NUNIS95)
+values
+('Fernanda', '2001', 'FEMININO', 'Rio de Janeiro', '6.5', '9.0', '6.0', '5.6', '5.8', '3.2', '9.7', '4.0', '8.0');
+
+select * from projeto_de_desenvolvimento_de_aplicação_para_tratamento_de_dados;
+delete from projeto_de_desenvolvimento_de_aplicação_para_tratamento_de_dados
+ where id = 4;
